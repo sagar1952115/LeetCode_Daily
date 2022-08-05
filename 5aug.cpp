55ae6d964039bdb9148706205dd3136e26d8d4fb
sagar1952115 <sagarkumar1952115@gmail.com>
2022-08-05 23:18:42 +0530

    final solution

--- 5aug.cpp
+++ 5aug.cpp
@@ -18,3 +18,44 @@
     int combinationSum4(vector<int>& nums, int target) {
        return solve(nums,target);
     }
+    
+    Memoisation--------------->
+    
+     int solve(vector<int>& nums, int target,vector<int> &dp)
+    {
+        if(target<0)
+            return 0;
+        if(target==0)
+            return 1;
+        if(dp[target]!=-1)
+            return dp[target];
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=solve(nums,target-nums[i],dp);
+        }
+        dp[target]=ans;
+        return dp[target];
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+       vector<int> dp(target+1,-1);
+       return solve(nums,target,dp);
+    }
+    
+    Top-down approach/Tabular approach------------------>
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target+1,0);
+        
+        dp[0]=1;
+        for(int  i=1;i<=target;i++)
+        {
+            for(auto it:nums)
+            {
+                if(i-it>=0)
+                dp[i]+=dp[i-it];
+            }
+        }
+        
+       return dp[target];
+    }
