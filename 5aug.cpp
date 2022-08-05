180854dc39b8218fc89c9a3ce0579eb5e13e9ff9
sagar1952115 <sagarkumar1952115@gmail.com>
2022-08-05 22:49:39 +0530

    recursive code

--- 5aug.cpp
+++ 5aug.cpp
@@ -1,5 +1,20 @@
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<"hello world";
-}
+ Recursion --------->
+ Will give TLE in long test cases.
+ time complexity is pow(2,n)
+ 
+ int solve(vector<int>& nums, int target)
+    {
+        if(target<0)
+            return 0;
+        if(target==0)
+            return 1;
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            ans+=solve(nums,target-nums[i]);
+        }
+        return ans;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+       return solve(nums,target);
+    }
